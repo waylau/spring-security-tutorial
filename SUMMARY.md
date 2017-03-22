b70ba64034591cf07916d5fd4603ab62c4420d16
waylau <waylau521@gmail.com>
2017-03-22 14:01:33 +0800

    init jwt

--- SUMMARY.md
+++ SUMMARY.md
@@ -15,7 +15,7 @@ This is the summary of my book.
 * [通用密码加密](dosc/password-encoder.md)
 * [Remember-Me（记住我）认证：基于散列的令牌方法](dosc/remember-me-hash.md)
 * [Remember-Me（记住我）认证：基于持久化的令牌方法](dosc/remember-me-persistent.md)
-* 基于 JWT 的认证
+* [基于 JWT 的认证](dosc/jwt-authentication.md)
 * 基于 OAuth2.0 的认证
 * [参考资料](docs/references.md)
 * To be continued ...未完待续...
---
+++ samples/jwt-authentication/src/main/java/com/waylau/spring/boot/security/config/SecurityConfig.java
@@ -0,0 +1,89 @@
+package com.waylau.spring.boot.security.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+
+/**
+ * Spring Security 配置类.
+ * 
+ * @since 1.0.0 2017年3月8日
+ * @author <a href="https://waylau.com">Way Lau</a>
+ */
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true) // 启用方法安全设置
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	private static final String KEY = "waylau.com";
+	
+	@Autowired
+	private UserDetailsService userDetailsService;
+
+	@Autowired
+    private PasswordEncoder passwordEncoder;
+ 
+	@Autowired
+	private DataSource dataSource;
+	
+	@Bean  
+	public JdbcTokenRepositoryImpl tokenRepository() {
+		JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
+		tokenRepository.setCreateTableOnStartup(true);   // 启动时自动建表，但重启数据会丢失
+		tokenRepository.setDataSource(dataSource);
+		return tokenRepository;
+	};
+	
+	@Bean  
+    public PasswordEncoder passwordEncoder() {  
+        return new BCryptPasswordEncoder();   // 使用 BCrypt 加密
+    }  
+	
+	@Bean  
+    public AuthenticationProvider authenticationProvider() {  
+		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+		authenticationProvider.setUserDetailsService(userDetailsService);
+		authenticationProvider.setPasswordEncoder(passwordEncoder); // 设置密码加密方式
+        return authenticationProvider;  
+    }  
+ 
+	/**
+	 * 自定义配置
+	 */
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/css/**", "/js/**", "/fonts/**", "/index").permitAll() // 都可以访问
+				.antMatchers("/h2-console/**").permitAll() // 都可以访问
+				.antMatchers("/users/**").hasRole("USER") // 需要相应的角色才能访问
+				.antMatchers("/admins/**").hasRole("ADMIN") // 需要相应的角色才能访问
+				.and()
+				.formLogin()   //基于 Form 表单登录验证
+				.loginPage("/login").failureUrl("/login-error") // 自定义登录界面
+				.and().rememberMe().key(KEY).tokenRepository(tokenRepository()) // 启用 remember me
+				.and().exceptionHandling().accessDeniedPage("/403");  // 处理异常，拒绝访问就重定向到 403 页面
+		http.csrf().ignoringAntMatchers("/h2-console/**"); // 禁用 H2 控制台的 CSRF 防护
+		http.headers().frameOptions().sameOrigin(); // 允许来自同一来源的H2 控制台的请求
+	}
+	
+	/**
+	 * 认证信息管理
+	 * @param auth
+	 * @throws Exception
+	 */
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService);
+		auth.authenticationProvider(authenticationProvider());
+	}
+}
